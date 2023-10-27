65693fab901a47d644a5b6d2a75757c7bbe760de
YuJin Kwon <tokyj515@naver.com>
2023-10-27 19:27:33 +0900

    [feat] 카카오앱키 변경 / DAN-29

--- src/main/java/com/example/dango/user/service/UserService.java
+++ src/main/java/com/example/dango/user/service/UserService.java
@@ -38,8 +38,6 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
 
-    private final CustomUserDetailsService customUserDetailsService;
-
 
 
     @SneakyThrows
