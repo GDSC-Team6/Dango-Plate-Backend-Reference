fd0399fb72261a4dfc6c28b269343698716ebf4c
Feedbaek <nicek789@naver.com>
2023-11-16 02:53:07 +0900

    Feat: KakaoMap 단위 테스트 추가

--- src/test/java/com/example/dango/DangoApplicationTests.java
+++ src/test/java/com/example/dango/DangoApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.dango;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DangoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
