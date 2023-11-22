b59fda9cf2da08ee5e5f2e607cd844d3c3f9874a
YuJin Kwon <tokyj515@naver.com>
2023-11-22 19:20:57 +0900

    [feat] 소셜로그인 리다이렉트 uri 수정

--- src/main/java/com/example/dango/user/controller/AuthController.java
+++ src/main/java/com/example/dango/user/controller/AuthController.java
@@ -25,7 +25,7 @@ public class AuthController {
      * */
     @ApiOperation(value = "카카오 로그인",
             notes = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=b7efcdeef9cf0be8991e8d2fdc1dc2ba&redirect_uri=http://localhost:8080/oauth/kakao\n" +
-                    "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=b7efcdeef9cf0be8991e8d2fdc1dc2ba&redirect_uri=http://35.216.0.111:8080/oauth/kakao\n")
+                    "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=b7efcdeef9cf0be8991e8d2fdc1dc2ba&redirect_uri=dangoplate://oauth\n")
     @GetMapping("/kakao")
     public ApiResponse<TokenRes> getAccessTokenKakao(@RequestParam String code) {
         String accessTokenFromSocial = authService.getKakaoAccessToken(code);
